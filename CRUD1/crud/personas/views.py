e2ac770422198968da2e35a76803ee5d297a9bfa
Viviana Luna <90412539+CatSnoopy@users.noreply.github.com>
2024-05-29 23:17:43 -0500

    Update views.py

--- CRUD1/crud/personas/views.py
+++ CRUD1/crud/personas/views.py
@@ -31,11 +31,17 @@ def crear_personas(request):
     correo_nuevo = request.POST('correo'),
     telefono_nuevo = request.POST('telefono'),
     ocupacion_nuevo = request.POST('ocupacion'))
-    
-    return render(
-        request, "formulario.html",{"personas": Personas, "error": ""}
-    )
-    personas_nuevas.save()
-    return redirect('/personas/')
+    if not (documento_nuevo and nombre_nuevo and apellidos_nuevo and fecha_nacimiento_nuevo and ciudad_nuevo and correo_nuevo and telefono_nuevo and ocupacion_nuevo):
+            # Verificar si algún campo obligatorio está vacío
+            return render(
+                request, "formulario_persona.html", {"error": "Todos los campos son obligatorios"}
+            )
 
+
+def delete(request, personas_id):
+    personas = Personas.objects.get(id=personas_id)
+    personas.delete()
+    return redirect("/tasks/")
+
+ 
 
