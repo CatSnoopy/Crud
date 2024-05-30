68bb6067440871af46280663891d0b99b394cd33
Viviana Luna <90412539+CatSnoopy@users.noreply.github.com>
2024-05-30 04:27:53 -0500

    Update urls.py

--- CRUD1/crud/personas/urls.py
+++ CRUD1/crud/personas/urls.py
@@ -7,8 +7,7 @@ from personas import views
 urlpatterns=[
     path('', views.formulario),
     path('new/', views.crear_personas, name='crear_personas'),
-    #path('delete_task/<int:task_id>/', views.delete_task, name='delete_task'),
-    #path('formulario/', views.Formulario, name='formulario'),
-    #path('',include(router.urls))
+    path('delete_persona/<int:persona_id>/', views.delete_persona, name='delete_persona'),
+]
     
 ]
