d18fe9a291947cfcfc1e848ae7f79ff490876740
Viviana Luna <90412539+CatSnoopy@users.noreply.github.com>
2024-06-15 22:24:10 -0500

    Delete CRUD1/crud/personas directory

--- CRUD1/crud/personas/form.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Persona
-
-class PersonaForm(forms.ModelForm):
-    class Meta:
-        model = Persona
-        fields = ['nombre', 'fecha_nacimiento', 'ciudad', 'correo', 'telefono', 'ocupacion']
