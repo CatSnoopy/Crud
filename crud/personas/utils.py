065909b397f9cf1a65067785f654d7bba55559b1
Viviana Luna <90412539+CatSnoopy@users.noreply.github.com>
2024-06-20 09:10:24 -0500

    Delete crud/personas directory

--- crud/personas/utils.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-def es_viable(fecha_nacimiento):
-    hoy = datetime.now().date()
-    edad = hoy.year - fecha_nacimiento.year - ((hoy.month, hoy.day) < (fecha_nacimiento.month, fecha_nacimiento.day))
-    return "Viable" if 18 <= edad <= 65 else "No viable"
