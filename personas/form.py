c031cc5b874de50749026660e387b1d5ee579214
Viviana Luna <90412539+CatSnoopy@users.noreply.github.com>
2024-06-27 14:50:18 -0500

    Delete personas directory

--- personas/form.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Personas
-
-class personaForm(forms.ModelForm):
-    class Meta:
-        model= Personas
-        exclude=[]
-        fields='nombre','apellidos','fecha_nacimiento','ciudad','correo','telefono','ocupacion'
